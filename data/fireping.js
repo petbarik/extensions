7b36596476a76dc4fad0d1932ad17fe5edd08c44
petbarik <150723555+petbarik@users.noreply.github.com>
2025-11-05 21:35:58 +0100

    Update fireping.js

--- data/fireping.js
+++ data/fireping.js
@@ -15,15 +15,27 @@ class FirebaseData {
           arguments: {
             URL: { type: 'string', defaultValue: 'https://yourdatabase.your_region.firebasedatabase.app/' },
             API: { type: 'string', defaultValue: 'AIza...' }
+          },
+          opcode: 'createUser',
+          blockType: 'command',
+          text: 'create user with email [EMAIL] password [PASSWORD] and username [USERNAME]',
+          arguments: {
+            EMAIL: { type: 'string', defaultValue: 'yourname@provider.com' },
+            PASSWORD: { type: 'string', defaultValue: 'Secret123' },
+            USERNAME: { type: 'string', defaultValue: 'yourusername' }
           }
         },
       ]
     };
   }
-  async setFireBase({ URL, API }) {
+  setFireBase({ URL, API }) {
     this.dataBaseURL = ${URL};
     this.APIkey = ${API};
   }
+
+  async createUser({ EMAIL, PASSWORD, USERNAME }) {
+    const res = await fetch("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + this.APIkey)
+  } 
 }
 
 Scratch.extensions.register(new FirebaseData());
