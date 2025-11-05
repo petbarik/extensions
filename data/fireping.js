2cb9e4327a0a309ab29e92f9e5aee8aa2454df4d
petbarik <150723555+petbarik@users.noreply.github.com>
2025-11-05 21:19:37 +0100

    Update fireping.js

--- data/fireping.js
+++ data/fireping.js
@@ -1,44 +1,28 @@
 class FirebaseData {
+  constructor() {
+    this.dataBaseURL = "";
+    this.APIkey = "";
+  }
   getInfo() {
     return {
       id: 'firebaseData',
       name: 'Firebase Data',
       blocks: [
         {
-          opcode: 'saveData',
-          blockType: Scratch.BlockType.COMMAND,
-          text: 'save key [KEY] value [VALUE]',
+          opcode: 'setFireBase',
+          blockType: 'command',
+          text: 'set database url to [URL] and API to [API]',
           arguments: {
-            KEY: { type: Scratch.ArgumentType.STRING, defaultValue: 'playerScore' },
-            VALUE: { type: Scratch.ArgumentType.STRING, defaultValue: '100' }
+            URL: { type: 'string', defaultValue: 'https://yourdatabase.your_region.firebasedatabase.app/' },
+            API: { type: 'string', defaultValue: 'AIza...' }
           }
         },
-        {
-          opcode: 'loadData',
-          blockType: Scratch.BlockType.REPORTER,
-          text: 'load key [KEY]',
-          arguments: {
-            KEY: { type: Scratch.ArgumentType.STRING, defaultValue: 'playerScore' }
-          }
-        }
       ]
     };
   }
-
-  async saveData(args) {
-    const dbUrl = 'https://penguinmod-games-default-rtdb.firebaseio.com/';
-    await fetch(`${dbUrl}${args.KEY}.json`, {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(args.VALUE)
-    });
-  }
-
-  async loadData(args) {
-    const dbUrl = 'https://penguinmod-games-default-rtdb.firebaseio.com/';
-    const res = await fetch(`${dbUrl}${args.KEY}.json`);
-    const data = await res.json();
-    return data ?? '';
+  async setFireBase({ URL, API }) {
+    this.dataBaseURL = ${URL};
+    this.APIkey = ${API};
   }
 }
 
